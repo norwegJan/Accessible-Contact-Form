a71d17cc7cdd3ec12f738a187936da8f91f85ce7
norwegJan <236618355+norwegJan@users.noreply.github.com>
2026-06-16 19:57:06 +0200

    started building out validation logic

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const mainForm = document.querySelector("#main-form");
 const submitBtn = document.querySelector("#submit-btn");
 
+const nameInputs = document.querySelectorAll(".name-input");
+const emailInput = document.querySelector("#email");
+const queryInputs = document.querySelectorAll(".query-input");
+const messageInput = document.querySelector("#message");
+const consentInput = document.querySelector("#consent");
+
 const errorMsgFirstName = document.querySelector("#error-msg-first-name");
 
 const errorMsg = {
@@ -12,8 +18,34 @@ const errorMsg = {
 
 mainForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const firstNameValue = nameInputs[0].value;
+  const lastNameValue = nameInputs[1].value;
+  const emailValue = emailInput.value;
+  const queryValue = getQuery();
+  const messageValue = messageInput.value;
+  const consentValue = getConsent();
+
+  console.log(
+    `Full name: ${firstNameValue} ${lastNameValue}
+Email Address: ${emailValue}
+Query Type: ${queryValue}
+Message: ${messageValue}
+Consent: ${consentValue}`,
+  );
 });
 
-submitBtn.addEventListener("click", () => {
-  console.log("Hokus Pokus, Filiokus! 🪄🧙‍♂️");
-});
+function isEmailValid() {}
+
+function getQuery() {
+  const queryArray = Array.from(queryInputs);
+  const selectedQuery = queryArray.find((radio) => radio.checked);
+
+  if (!selectedQuery) return null;
+  return selectedQuery.value;
+}
+
+function getConsent() {
+  if (!consentInput.checked) return "NO";
+  return consentInput.value;
+}
